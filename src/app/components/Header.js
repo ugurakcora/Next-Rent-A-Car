46af14ed45cafd1d458eaebe9739bd246cb81b71
ugurakcora <uakcora97@gmail.com>
2023-09-15 02:29:12 +0300

    start header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -14,6 +14,9 @@ import { SearchMobile } from "./SearchMobile";
 // Media Query Hook
 import { useMediaQuery } from "react-responsive";
 
+// Icons
+import { BiMenuAltRight, BiX } from "react-icons/bi";
+
 export default function Header() {
   const [header, setHeader] = useState(false);
   const [nav, setNav] = useState(false);
@@ -23,6 +26,7 @@ export default function Header() {
   });
 
   useEffect(() => {
+    // Header
     console.log(desktopMode);
     const handleScroll = () => {
       if (window.scrollY > 40) {
@@ -43,5 +47,59 @@ export default function Header() {
 
   console.log(header);
 
-  return <div>Header</div>;
+  return (
+    <header
+      className={`${
+        header ? "bg-white shadow-md py-2" : "bg-transparent shadow-none py-4"
+      } fixed w-full max-w-[1920px] mx-auto z-20 transition-all duration-300`}
+    >
+      <div className="mx-auto flex flex-col xl:container xl:flex-row xl:items-center xl:justify-between">
+        <div className="flex justify-between items-center px-4">
+          {/* Logo */}
+          <Link
+            to="home"
+            smooth={desktopMode}
+            spy={true}
+            className="cursor-pointer"
+          >
+            <Image src={"icons/logo.svg"} alt="Logo" width={194} height={64} />
+          </Link>
+
+          {/* Nav Open Menu */}
+          <div
+            onClick={() => setNav(!nav)}
+            className="cursor-pointer xl:hidden"
+          >
+            {nav ? (
+              <BiX className="text-4xl" />
+            ) : (
+              <BiMenuAltRight className="text-4xl" />
+            )}
+          </div>
+
+          {/* Nav */}
+          <nav className="hidden xl:flex items-center space-x-8">
+            <Link
+              to="home"
+              smooth={desktopMode}
+              spy={true}
+              activeClass="active"
+              className="cursor-pointer"
+            >
+              Home
+            </Link>
+            <Link
+              to="cars"
+              smooth={desktopMode}
+              spy={true}
+              activeClass="active"
+              className="cursor-pointer"
+            >
+              Cars
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
