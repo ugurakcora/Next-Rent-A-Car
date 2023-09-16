2e3d7f4420c98780fc621bfd2d5cfec97ea58ee2
ugurakcora <uakcora97@gmail.com>
2023-09-16 15:49:42 +0300

    init search mobile

--- src/app/components/SearchMobile.js
+++ src/app/components/SearchMobile.js
@@ -1,3 +1,27 @@
+// Componets
+import DateSelection from "./DateSelection";
+import HoursSelection from "./HoursSelection";
+import LocationSelection from "./LocationSelection";
+
 export default function SearchMobile() {
-  return <div>SearchMobile</div>;
+  return (
+    <div className="xl:hidden font-medium">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-4">
+          {/* Location */}
+          <LocationSelection />
+          {/* Date */}
+          <DateSelection />
+          {/* Hours */}
+          <HoursSelection />
+          {/* Button */}
+          <div className="flex items-center px-6">
+            <button className="btn btn-accent btn-sm w-[164px] mx-auto">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
