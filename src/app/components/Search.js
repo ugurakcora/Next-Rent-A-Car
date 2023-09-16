08574d2630dfdec948ca5422e46d538083316407
ugurakcora <uakcora97@gmail.com>
2023-09-16 13:55:28 +0300

    finished hero

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -1,7 +1,11 @@
 "use client";
 
+// Search Context
 import { useContext } from "react";
 import { SearchContext } from "../context/search";
+
+// Components
+import LocationSelection from "./LocationSelection";
 
 export default function Search() {
   const { searchActive } = useContext(SearchContext);
@@ -13,7 +17,9 @@ export default function Search() {
           : "bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]"
       } hidden xl:block w-full relative shadow-lg`}
     >
-      Search
+      <div className="xl:h-full flex items-center px-6 xl:px-0">
+        <LocationSelection />
+      </div>
     </div>
   );
 }
